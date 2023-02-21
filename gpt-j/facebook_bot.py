b71b0f0af95ebddd2617ae1c11e019cf17c7e6f5
chang <changlee99@gmail.com>
2023-02-21 20:57:25 +0900

    facebook messenger add

---
+++ gpt-j/facebook_bot.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import json
+from datetime import datetime
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# tunning facebook web hook to local
+# ssh -R 8091:127.0.0.1:5000 lcw.plan4.house
+
+fb_veryfy_token = os.environ["FB_VERIFY_TOKEN"]
+fb_page_access_token = os.environ["FB_PAGE_ACCESS_TOKEN"]
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == fb_veryfy_token:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+                    is_echo = False
+                    if "is_echo" in messaging_event["message"]:
+                        is_echo = messaging_event["message"]["is_echo"]
+                    if not is_echo:
+                        send_message(sender_id, f"echo {message_text}")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": fb_page_access_token
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(msg):  # simple wrapper for logging to stdout on heroku
+    if type(msg) is dict:
+        msg = json.dumps(msg, ensure_ascii=False)
+    else:
+        msg = str(msg)
+    print(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- gpt-j/temp.py
+++ gpt-j/temp.py
@@ -1,24 +1,41 @@
-from transformers import AutoTokenizer, logging, pipeline, AutoModelForCausalLM
-import torch, deepspeed
-from deepspeed.module_inject.containers.gptneox import DS_GPTNEOXContainer, GPTNEOXLayerPolicy
-from transformers import GPTNeoXLayer
+server {
+        listen 80;
+        listen [::]:80;
+        server_name messenger.plan4.house;
 
-latest_model_dir_on_test = "/home/chang/AI/llm/t5tests/gpt-j/Models/polyglot-ko-3.8b-multi-func/checkpoint-2840"
-gpt_on_test = AutoModelForCausalLM.from_pretrained(
-    latest_model_dir_on_test,
-    torch_dtype=torch.float16,
-    low_cpu_mem_usage=True,
-    #load_in_8bit=True,
-    #device_map='auto',
-).to('cuda:0', torch.float16)
+        location ~ /.well-known {
+               root /home/sunnyville/content/well;
+        }
+        location / {
+                return 301 https://messenger.plan4.house;
+        }
+}
 
-ds_engine = deepspeed.init_inference(
-    gpt_on_test,
-    mp_size=1,
-    dtype=torch.float16,
-    replace_method='auto',
-    checkpoint=None,
-    replace_with_kernel_inject=False,
-    injection_policy={GPTNeoXLayer: (GPTNEOXLayerPolicy, )}
-)
-    
+server {
+        listen 443 ssl http2;
+        listen [::]:443 ssl http2;
+
+        server_name messenger.plan4.house;
+
+        ssl_certificate      /etc/letsencrypt/live/plan4.house/fullchain.pem;
+        ssl_certificate_key  /etc/letsencrypt/live/plan4.house/privkey.pem;
+	ssl_trusted_certificate /etc/letsencrypt/live/plan4.house/fullchain.pem;
+
+        location ~ /.well-known {
+               root /home/sunnyville/content/well;
+        }
+        location / {
+		proxy_set_header Host $host;
+		proxy_set_header X-Real-IP $remote_addr;
+
+		proxy_set_header X-Forwarded-Host $host;
+		proxy_set_header X-Forwarded-Server $host;
+		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+		proxy_http_version 1.1;
+
+		proxy_pass http://localhost:8091;
+		proxy_redirect off;
+		proxy_buffering off;
+        }
+}
