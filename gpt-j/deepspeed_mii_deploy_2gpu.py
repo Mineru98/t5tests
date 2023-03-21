4d6ae40f963fc3174887322b200eb1477426cd8b
lcw99 <changlee99@gmail.com>
2023-03-21 16:34:23 +0900

    aa

--- gpt-j/deepspeed_mii_deploy_2gpu.py
+++ gpt-j/deepspeed_mii_deploy_2gpu.py
@@ -46,7 +46,7 @@ ds_config2 = {
     },
     "train_micro_batch_size_per_gpu": 1,
 }
-model_path = "/home/chang/hd3t/t5tests/gpt-j/Models/polyglot-ko-3.8b-lora-merged-based-v2-4640"
+model_path = "/home/chang/hd3t/t5tests/gpt-j/Models/polyglot-ko-3.8b-alpaca/checkpoint-20"
 
 mii.deploy(task="text-generation",
            model=model_path,
