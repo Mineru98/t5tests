40a48c98ad7c3cb6c24303dce9a6e57ee90a68cf
chang <changlee99@gmail.com>
2023-03-22 21:25:37 +0900

    alpaca

--- gpt-j/deepspeed_mii_deploy_2gpu.py
+++ gpt-j/deepspeed_mii_deploy_2gpu.py
@@ -46,7 +46,8 @@ ds_config2 = {
     },
     "train_micro_batch_size_per_gpu": 1,
 }
-model_path = "/home/chang/hd3t/t5tests/gpt-j/Models/polyglot-ko-3.8b-alpaca/checkpoint-20"
+import os
+model_path = os.environ["CURRENT_MODEL"]
 
 mii.deploy(task="text-generation",
            model=model_path,
