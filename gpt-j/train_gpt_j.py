8fed1f6bc8a73a1d74fa2c73332da53e16f42265
chang <changlee99@gmail.com>
2023-03-29 20:29:53 +0900

    aa

--- gpt-j/train_gpt_j.py
+++ gpt-j/train_gpt_j.py
@@ -1459,7 +1459,7 @@ def huggingface_trainer():
     print(args)
     
     hf_trainer = MyTrainer
-    if load_in_8bit:
+    if load_in_8bit and not is_ds:
         hf_trainer = Trainer
         
     data_collator = DataCollatorForLanguageModeling(tokenizer, return_tensors="pt", mlm=False)
@@ -1474,7 +1474,7 @@ def huggingface_trainer():
         preprocess_logits_for_metrics = preprocess_logits_for_metrics
     )
 
-    if not load_in_8bit:
+    if not load_in_8bit or is_ds:
         trainer, model, optimizer, lr_scheduler, train_dataloader, eval_dataloader = accelerator.prepare(
             trainer, model, optimizer, lr_scheduler, train_dataloader, eval_dataloader
         )
