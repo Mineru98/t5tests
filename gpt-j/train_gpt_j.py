9ca1a9b2297482b420e1751c5a4ac0985df845c3
lcw99 <changlee99@gmail.com>
2023-03-30 12:01:00 +0900

    trans data added to llama

--- gpt-j/train_gpt_j.py
+++ gpt-j/train_gpt_j.py
@@ -673,14 +673,14 @@ def get_dataset(tokenize):
         dss_eval.append(ds_eval)
         dss_train.append(ds_train)        
     if "alpaca_ko_to_en" in dataset_source.keys():
-        ds = load_dataset("json", data_files={'train': f"{data_server}alpaca_data_kr_checked.zip"}, download_mode='force_redownload')
+        ds = load_dataset("json", data_files={'train': f"{data_server}alpaca_data_kr_checked.zip"})
         text_templates = text_templates_alpaca_ko_to_en
         source = "alpaca_ko_to_en"
         ds_eval, ds_train = preprocess_dataset(source, dataset_source[source], ds, tokenize)
         dss_eval.append(ds_eval)
         dss_train.append(ds_train)        
     if "alpaca_en_to_ko" in dataset_source.keys():
-        ds = load_dataset("json", data_files={'train': f"{data_server}alpaca_data_kr_checked.zip"}, download_mode='force_redownload')
+        ds = load_dataset("json", data_files={'train': f"{data_server}alpaca_data_kr_checked.zip"})
         text_templates = text_templates_alpaca_en_to_ko
         source = "alpaca_en_to_ko"
         ds_eval, ds_train = preprocess_dataset(source, dataset_source[source], ds, tokenize)
@@ -694,7 +694,7 @@ def get_dataset(tokenize):
         dss_eval.append(ds_eval)
         dss_train.append(ds_train)        
     if "tarot_conv" in dataset_source.keys():
-        ds = load_dataset("json", data_files={'train': f"{data_server}tarot_conv_text.zip"}, download_mode='force_redownload')
+        ds = load_dataset("json", data_files={'train': f"{data_server}tarot_conv_text.zip"})
         text_templates = ["{s['text']}"]
         source = "tarot_conv"
         ds_eval, ds_train = preprocess_dataset(source, dataset_source[source], ds, tokenize)
