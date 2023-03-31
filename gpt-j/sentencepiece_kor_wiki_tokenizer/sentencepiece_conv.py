82440e0b4feeb0108a5af408e4a4dcec37c37e42
chang <changlee99@gmail.com>
2023-03-31 11:43:10 +0900

    aa

--- gpt-j/sentencepiece_kor_wiki_tokenizer/sentencepiece_conv.py
+++ gpt-j/sentencepiece_kor_wiki_tokenizer/sentencepiece_conv.py
@@ -1,4 +1,5 @@
 import sentencepiece_model_pb2 as model
+from transformers import LlamaForCausalLM, LlamaTokenizer
 
 m_org = model.ModelProto()
 m_org.ParseFromString(open('./StockModels/llama-7B-origianal/tokenizer.model', 'rb').read())
@@ -19,3 +20,5 @@ for k_piece in m_kor.pieces:
 print(f"{len(m_org.pieces)=}")
 with open('./StockModels/llama_kor_entended_tokenizer/tokenizer.model', 'wb') as f:
     f.write(m_org.SerializeToString())
+    
+tokenizer = LlamaTokenizer.from_pretrained('./StockModels/llama_kor_entended_tokenizer/tokenizer.model')
